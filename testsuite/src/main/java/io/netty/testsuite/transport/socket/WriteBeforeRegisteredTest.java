8bf142b80778fefbe3672c4330d5344c49c72baa
Norman Maurer <norman_maurer@apple.com>
2025-11-20 15:28:00 +0100

    Fix test

--- testsuite/src/main/java/io/netty/testsuite/transport/socket/WriteBeforeRegisteredTest.java
+++ testsuite/src/main/java/io/netty/testsuite/transport/socket/WriteBeforeRegisteredTest.java
@@ -44,7 +44,7 @@ public class WriteBeforeRegisteredTest extends AbstractClientSocketTest {
         TestHandler h = new TestHandler();
         SocketChannel ch = null;
         try {
-            ch = (SocketChannel) cb.handler(h).connect(newSocketAddress()).channel();
+            ch = (SocketChannel) cb.handler(h).register().sync().channel().connect(newSocketAddress()).channel();
             ch.writeAndFlush(randomBufferType(ch.alloc(), new byte[] { 1 }, 0, 1));
         } finally {
             if (ch != null) {
