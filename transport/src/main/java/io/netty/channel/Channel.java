895d588a88a5c5402d21593d709fb7e4b499ecd0
Norman Maurer <norman_maurer@apple.com>
2025-11-20 10:23:26 +0100

    Merge IoEventLoop into EventLoop and IoEventLoopGroup into EventLoopGroup
    Motivation:
    
    We introduced the IoEventLoop and IoEventLoopGroup interfaces into 4.2 to be able to rewrite how our EventLoops work in 4.2. This was just done to not break the API, in 5.0 we should just merge both to simplify stuff.
    
    Modifications:
    
    Merge EventLoop* interfaces
    
    Result:
    
    Cleanup and preparation for how Channels will be registered / assign to an EventLoop

--- transport/src/main/java/io/netty/channel/Channel.java
+++ transport/src/main/java/io/netty/channel/Channel.java
@@ -353,6 +353,25 @@ public interface Channel extends AttributeMap, ChannelOutboundInvoker, Comparabl
         return pipeline().newFailedFuture(cause);
     }
 
+    // These are just added for now and will be removed in the followup PR.
+    default ChannelFuture register(EventLoop loop) {
+        DefaultChannelPromise promise = new DefaultChannelPromise(this, loop);
+        register(loop, promise);
+        return promise;
+    }
+
+    // These are just added for now and will be removed in the followup PR.
+    default ChannelFuture register(EventLoop loop, ChannelPromise promise) {
+        if (loop.inEventLoop()) {
+            unsafe().register(loop, promise);
+        } else {
+            loop.execute(() -> {
+                unsafe().register(loop, promise);
+            });
+        }
+        return promise;
+    }
+
     /**
      * <em>Unsafe</em> operations that should <em>never</em> be called from user-code. These methods
      * are only provided to implement the actual transport, and must be invoked from an I/O thread except for the
